f3589bf63ebe5550530677be29db5bc2dc491c2b
Himanshu Gupta <himanshugupta00072@gmail.com>
2026-05-20 18:10:58 +0530

    clean upload

---
+++ app/contact/edit/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const EditContactPage = () => {
+    return (
+        <div>
+EditPAges
+        </div>
+    );
+};
+export default EditContactPage;
---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+const contact = () => {
+    return (
+        <div>
+            HEllo how You Doing welcome in the cotact pages.
+
+        </div>
+    );
+};
+
+export default contact;
